a0af900f6370455cc8079b4ff9d82906624aab9c
Alexandre DeZotti <dezotti.alexandre@gmail.com>
2025-04-08 20:21:04 +0200

    embedder works

--- embed-data-into-html.py
+++ embed-data-into-html.py
@@ -9,8 +9,8 @@ with open('html/search.html', 'r', encoding='utf-8') as html_file:
 with open('css/dict.css', 'r', encoding='utf-8') as css_file:
     css_content = css_file.read()
 
-html_content.replace('<link rel="stylesheet" href="css/dict.css">', css_content)
-html_content.replace('const embedded_data = [];', 'const embedded_data = ' + json.dumps(data, ensure_ascii=False) + ';')
+html_content = html_content.replace('<link rel="stylesheet" href="css/dict.css">', '<style>'+css_content+'</style>')
+html_content = html_content.replace('const embedded_data = false;', 'const embedded_data = ' + json.dumps(data, ensure_ascii=False) + ';')
 
 
 with open('html/search-en-de.html', 'w', encoding='utf-8') as html_file:
