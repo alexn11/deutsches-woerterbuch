21900082f8aa3e27167ac19c9bb0d8db0b2f475d
Alexandre DeZotti <dezotti.alexandre@gmail.com>
2025-04-09 12:58:47 +0200

    exact language code conversion

--- wikitools/wiki_urls.py
+++ wikitools/wiki_urls.py
@@ -1,15 +1,12 @@
+import iso639
 
 def convert_language_code_to_language_name(language_code: str) -> str:
-    return {
-        'en': 'English',
-        'fr': 'French',
-        'es': 'Spanish',
-        'de': 'German',
-        'it': 'Italian',
-        'ja': 'Japanese',
-        'zh': 'Chinese',
-        # Add more language codes and names as needed
-    }.get(language_code, language_code)  # Default to the code itself if not found
+    lang_data = iso639.find(iso639_1=language_code)
+    if(lang_data is None):
+        language_name = language_code
+    else:
+        language_name = lang_data['name']
+    return language_name
 
 def make_url(entry: str, language: str|None = None, language_code: str|None = None) -> str:
     url = f'https://en.wiktionary.org/wiki/{entry}'
